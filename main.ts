15417a01c6d50c6fef643c395b69d8841f73eb9c
Sean Bowers <sean.t.bowers@gmail.com>
2022-01-26 10:26:13 -0700

    Initial Version

--- main.ts
+++ main.ts
@@ -1,81 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownPostProcessor, MarkdownPostProcessorContext, MarkdownRenderChild, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CBLSettings {
+	showLanguageAsLabel: boolean;
+	forceReload: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CBLSettings = {
+	showLanguageAsLabel: true,
+	forceReload: true,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const REGEX_CODEBLOCK_LABEL = /^```(?<lang>[^\s]+)?\s*(?:{(?<label>[^}]+)})?/
+
+export default class CBLPlugin extends Plugin {
+	settings: CBLSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// filename => (position, cbTop)
+		var cache: Map<string, {}>;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerMarkdownPostProcessor((el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+			var section = ctx.getSectionInfo(el);
+			if (section == null) { return; }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			// Get the text that corresponds to this section
+			var lines = section.text.split("\n").map(s => s.trim());
+			var raw = lines.slice(section.lineStart, section.lineEnd + 1);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			// Check if this is a code block, and if not return early
+			if (raw.length < 2 || !raw[0].startsWith("```") || !raw.last().endsWith("```")) { return; }
+
+			// Look for a label on the first line
+			var match = raw[0].match(REGEX_CODEBLOCK_LABEL);
+			if (match == null) { return; }
+
+			// Get capture groups
+			var lang = match.groups["lang"];
+			var label = match.groups["label"];
+
+			console.log(`Code block: ${lang} -- ${label}`);
+
+			// Determine what to show in the label
+			var labelText = null;
+			// Prefer to show an explicitly set label
+			if (label != null) { labelText = label; }
+			// Otherwise, if the showLanguageAsLabel setting is set, use the language if specified
+			else if (lang != null && this.settings.showLanguageAsLabel) { labelText = lang; }
+
+			// Return early if there's no label to show
+			if (labelText == null) { return; }
+
+			// Add a label element
+			var labelEl = document.createElement("p");
+			labelEl.setText(labelText);
+			labelEl.addClass("codeblock-label");
+			el.prepend(labelEl);
+			ctx.addChild(new MarkdownRenderChild(labelEl));
+		});
+
+		this.app.metadataCache.on("changed", file => {
+			if (!this.settings.forceReload) { return; }
+
+			// Reload the preview
+			var mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			mdView.previewMode.rerender(true);
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new CBLSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,46 +85,38 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CBLSettingTab extends PluginSettingTab {
+	plugin: CBLPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CBLPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Code Block Label Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Show Language as Label')
+			.setDesc('If no label is given but there is a set highlight language show that language as the label.')
+			.addToggle(tgl => tgl
+				.setValue(this.plugin.settings.showLanguageAsLabel)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.showLanguageAsLabel = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Force Reload on Source Change')
+			.setDesc('Forces the markdown preview to be reloaded whenever the markdown source is changed. This is required to pick up changes in the label, but may slow down obsidian by causing multiple reloads on some changes.')
+			.addToggle(tgl => tgl
+				.setValue(this.plugin.settings.forceReload)
+				.onChange(async (value) => {
+					this.plugin.settings.forceReload = value;
 					await this.plugin.saveSettings();
 				}));
 	}
