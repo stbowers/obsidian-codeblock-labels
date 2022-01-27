a93a8f91a7a64e48097a7d9ff8fe24231c0476b3
Sean Bowers <sean.t.bowers@gmail.com>
2022-01-27 15:22:52 -0700

    Cleaned up code

--- main.ts
+++ main.ts
@@ -8,7 +8,8 @@ const DEFAULT_SETTINGS: CBLSettings = {
     showLanguageAsLabel: true,
 }
 
-const REGEX_CODEBLOCK_LABEL = /^```(?<lang>[^\s]+)?\s*(?:{(?<label>[^}]+)})?/
+// Regex which matches the first line of a fenced codeblock and gets the info we need from it as capture groups
+const REGEX_FENCED_CODEBLOCK_START = /^```(?<lang>[^\s^{]+)?\s*(?:{(?<label>[^}]+)})?/
 
 export default class CBLPlugin extends Plugin {
     settings: CBLSettings;
@@ -16,49 +17,8 @@ export default class CBLPlugin extends Plugin {
     async onload() {
         await this.loadSettings();
 
-        // filename => (position, cbTop)
-        var cache: Map<string, {}>;
-
-        this.registerMarkdownPostProcessor((el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
-            var section = ctx.getSectionInfo(el);
-            if (section == null) { return; }
-
-            // Get the text that corresponds to this section
-            var lines = section.text.split("\n").map(s => s.trim());
-            var raw = lines.slice(section.lineStart, section.lineEnd + 1);
-
-            // Check if this is a code block, and if not return early
-            if (raw.length < 2 || !raw[0].startsWith("```") || !raw.last().endsWith("```")) { return; }
-
-            // Look for a label on the first line
-            var match = raw[0].match(REGEX_CODEBLOCK_LABEL);
-            if (match == null) { return; }
-
-            // Get capture groups
-            var lang = match.groups["lang"];
-            var label = match.groups["label"];
-
-            console.log(`Code block: ${lang} -- ${label}`);
-
-            // Determine what to show in the label
-            var labelText = null;
-            // Prefer to show an explicitly set label
-            if (label != null) { labelText = label; }
-            // Otherwise, if the showLanguageAsLabel setting is set, use the language if specified
-            else if (lang != null && this.settings.showLanguageAsLabel) { labelText = lang; }
-
-            // Return early if there's no label to show
-            if (labelText == null) { return; }
-
-            // Add a label element
-            var labelEl = document.createElement("p");
-            labelEl.setText(labelText);
-            labelEl.addClass("codeblock-label");
-            el.prepend(labelEl);
-            ctx.addChild(new MarkdownRenderChild(labelEl));
-        });
-
-        // This adds a settings tab so the user can configure various aspects of the plugin
+        // Register callbacks, settings tab, etc.
+        this.registerMarkdownPostProcessor((el, ctx) => this.markdownPostProcessor(el, ctx));
         this.addSettingTab(new CBLSettingTab(this.app, this));
     }
 
@@ -72,6 +32,45 @@ export default class CBLPlugin extends Plugin {
 
     async saveSettings() {
         await this.saveData(this.settings);
+    }
+
+    markdownPostProcessor(el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+        // Only modify codeblocks, which are rendered as a code tag inside a pre tag
+        if (!el.querySelector("pre > code")) { return; }
+
+        // Get the section info from ctx
+        var section = ctx.getSectionInfo(el);
+        if (section == null) { return; }
+
+        // Get the text that corresponds to this section, and make sure it's a fenced code block
+        var lines = section.text.split("\n")
+            .map(s => s.trim())
+            .slice(section.lineStart, section.lineEnd + 1);
+        if (lines.length < 2 || !lines[0].startsWith("```") || !lines.last().endsWith("```")) { return; }
+
+        // Match the start of the code block with the regex
+        var match = lines[0].match(REGEX_FENCED_CODEBLOCK_START);
+        if (match == null) { return; }
+
+        // Determine what to use for the label
+        // Use the label contents by default if set
+        var labelText = match.groups["label"];
+
+        // If an explicit label wasn't set but showLanguageAsLabel is true, use the language
+        if (labelText == null && this.settings.showLanguageAsLabel) {
+            labelText = match.groups["lang"];
+        }
+
+        // Return early if there's no label to show
+        if (labelText == null) { return; }
+
+        // Create the label element
+        var label = document.createElement("p");
+        label.addClass("codeblock-label");
+        label.setText(labelText);
+        el.prepend(label);
+        el.addClass("labeled-codeblock");
+        ctx.addChild(new MarkdownRenderChild(label));
     }
 }
 
